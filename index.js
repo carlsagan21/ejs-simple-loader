c6205d560b798cf8eb1e55edc54d33a81ea31010
soo <carlsagan96@gmail.com>
2017-02-26 01:51:19 +0900

    update package.json implement index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world!');
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author sookiwi
+*/
+var loaderUtils = require("loader-utils");
+var ejs = require("ejs")
+var path = require('path');
+
+module.exports = function(content) {
+  this.cacheable && this.cacheable();
+
+	var query = loaderUtils.getOptions(this) || {};
+	var configKey = query.config || "ejsSimpleLoader";
+	var options = this.options[configKey] || {};
+
+  // Use filenames relative to working dir, which should be project root
+  options.filename = path.relative(process.cwd(), this.resourcePath);
+
+  var template = ejs.compile(content, options)(query);
+
+  return 'module.exports = ' + JSON.stringify(template);
+}
