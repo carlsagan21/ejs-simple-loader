401f8fb1ce6dcbc8feae0bea2258470cbe463b1f
soo <carlsagan96@gmail.com>
2017-02-26 12:21:33 +0900

    "use strict"

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+"use strict";
+
 /*
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author sookiwi
 */
 var loaderUtils = require("loader-utils");
-var ejs = require("ejs")
+var ejs = require("ejs");
 var path = require('path');
 
-module.exports = function(content) {
+module.exports = function (content) {
   this.cacheable && this.cacheable();
 
-	var query = loaderUtils.getOptions(this) || {};
-	var configKey = query.config || "ejsSimpleLoader";
-	var options = this.options[configKey] || {};
+  var query = loaderUtils.getOptions(this) || {};
+  var configKey = query.config || "ejsSimpleLoader";
+  var options = this.options[configKey] || {};
 
   // Use filenames relative to working dir, which should be project root
   options.filename = path.relative(process.cwd(), this.resourcePath);
@@ -19,4 +21,4 @@ module.exports = function(content) {
   var template = ejs.compile(content, options)(query);
 
   return 'module.exports = ' + JSON.stringify(template);
-}
+};
